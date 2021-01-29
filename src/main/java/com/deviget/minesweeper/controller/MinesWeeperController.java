cf93b442dd966559bdefe1d4a6ffa72a4534c145
QuiqueScampini <enrique.scampini@despegar.com>
2021-01-29 18:04:04 -0300

    MinesWeeperController with message

---
+++ src/main/java/com/deviget/minesweeper/controller/MinesWeeperController.java
@@ -0,0 +1,16 @@
+package com.deviget.minesweeper.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/minesweeper")
+public class MinesWeeperController {
+
+	@GetMapping("/")
+	public String index() {
+		return "MinesWeeper Api";
+	}
+
+}
