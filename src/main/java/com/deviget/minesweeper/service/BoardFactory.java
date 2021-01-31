63689d15558335998bc96f60654e47ca7b524d8b
QuiqueScampini <enrique.scampini@despegar.com>
2021-01-31 17:42:37 -0300

    BoardFactory definition

---
+++ src/main/java/com/deviget/minesweeper/service/BoardFactory.java
@@ -0,0 +1,77 @@
+package com.deviget.minesweeper.service;
+
+import com.deviget.minesweeper.api.request.GameRequest;
+import com.deviget.minesweeper.model.Cell;
+import org.springframework.data.util.Pair;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@Component
+public class BoardFactory {
+
+	private final Random randomGenerator = new Random();
+
+	public Cell[][] create(GameRequest gameRequest) {
+		int cols = gameRequest.getCols();
+		int rows = gameRequest.getRows();
+		int mines = gameRequest.getMines();
+
+		List<Pair<Integer, Integer>> minesPositions = this.minesPositions(rows,cols,mines);
+
+		Cell[][] cells = new Cell[cols][rows];
+		for(int row = 0; row< rows; row++){
+			for(int col = 0; col< cols; col++){
+				cells[row][col] = this.createCell(row, col, minesPositions);
+			}
+		}
+		return cells;
+	}
+
+	private Cell createCell(int row, int col, List<Pair<Integer, Integer>>  minesPositions) {
+		boolean isMine = this.isMine(row, col, minesPositions);
+		long value = this.calculateValue(row, col, minesPositions);
+		return new Cell(isMine,value);
+	}
+
+	private boolean isMine(int row, int col, List<Pair<Integer, Integer>> minesPositions) {
+		return minesPositions.stream().anyMatch(minePosition ->
+				minePosition.getFirst().equals(row) && minePosition.getSecond().equals(col)
+		);
+	}
+
+	/**
+	 *	Having a list of mines positions and my row, col i can define my value
+	 *	From that list I keep the positions that surround me and count them.
+	 * **/
+	private long calculateValue(int row, int col, List<Pair<Integer, Integer>> minesPositions) {
+		return minesPositions.stream().filter(minePosition ->
+				minePosition.getFirst() >= row - 1 && minePosition.getFirst() <= row + 1 &&
+						minePosition.getSecond() >= col - 1 && minePosition.getSecond() <= col + 1
+		).count();
+	}
+
+
+	/**
+	 * This method gets the random position of the mines
+	 * 		1. We get the board size
+	 * 	  	2. We get 3 random numbers between 0 and 8
+	 * 	  	3. We calculate the (row,col) position in base of that numbers to create a list of Pairs
+	 * Ej:
+	 * 	We have 3 mines in a 3x3 board
+	 * 		1. 3*3= 9
+	 * 		2. For example (4,5,8)
+	 * 		3. 4 -> (1,1) , 5 -> (1,2) and 8 -> (2,2) being 1 -> (0,0).
+	 *
+	 * **/
+	private List<Pair<Integer, Integer>> minesPositions(int rows, int cols, int mines){
+		int boardSize = cols * rows;
+		return this.randomGenerator.ints(0, boardSize)
+				.distinct()
+				.limit(mines)
+				.mapToObj(positionNumber -> Pair.of(positionNumber / rows, positionNumber % cols))
+				.collect(Collectors.toList());
+	}
+}
---
+++ src/test/java/com/deviget/minesweeper/service/BoardFactoryTest.java
@@ -0,0 +1,76 @@
+package com.deviget.minesweeper.service;
+
+import com.deviget.minesweeper.api.request.GameRequest;
+import com.deviget.minesweeper.model.Cell;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+class BoardFactoryTest {
+
+	private static final int COLS = 3;
+	private static final int ROWS = 3;
+	private static final int BOARD_SIZE = COLS * ROWS;
+	private static final int MINES = 3;
+
+	@InjectMocks
+	private BoardFactory factory;
+
+	@Mock(answer = Answers.RETURNS_DEEP_STUBS)
+	private Random randomGenerator;
+
+	@Mock
+	private GameRequest request;
+
+	@BeforeEach
+	public void setup(){
+		openMocks(this);
+		ReflectionTestUtils.setField(factory,"randomGenerator",randomGenerator);
+		when(this.randomGenerator
+				.ints(0, BOARD_SIZE)
+				.distinct()
+				.limit(MINES)
+		).thenReturn(IntStream.of(4, 5, 8));
+	}
+
+	@Test
+	public void create() {
+		//Arrange
+		when(request.getRows()).thenReturn(ROWS);
+		when(request.getCols()).thenReturn(COLS);
+		when(request.getMines()).thenReturn(MINES);
+
+		//Act
+		Cell[][] cells = factory.create(request);
+
+		//Board assertion
+		assertEquals(ROWS,cells.length);
+		assertEquals(ROWS,cells[0].length);
+
+		//Row 0 assertion
+		assertEquals(1, cells[0][0].getValue());
+		assertEquals(2, cells[0][1].getValue());
+		assertEquals(2, cells[0][2].getValue());
+
+		//Row 1 assertion
+		assertEquals(1, cells[1][0].getValue());
+		assertTrue(cells[1][1].isMine());
+		assertTrue(cells[1][2].isMine());
+
+		//Row 2 assertion
+		assertEquals(1, cells[2][0].getValue());
+		assertEquals(3, cells[2][1].getValue());
+		assertTrue(cells[2][2].isMine());
+	}
+}
