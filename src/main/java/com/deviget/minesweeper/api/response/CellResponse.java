a982fb4ad5113000ebbcedf2a36a13b180fe8050
QuiqueScampini <enrique.scampini@despegar.com>
2021-01-31 20:33:34 -0300

    CellResponse Builder and Improve CellsResponseTransformer

--- src/main/java/com/deviget/minesweeper/api/response/CellResponse.java
+++ src/main/java/com/deviget/minesweeper/api/response/CellResponse.java
@@ -6,22 +6,11 @@ public class CellResponse {
 
 	int row;
 	int col;
-	private final boolean hidden;
 	private Integer value;
 	private CellContent content;
 
-	public CellResponse(int row, int col, CellContent content) {
-		this.row = row;
-		this.col = col;
-		this.hidden = true;
-		this.content = content;
-	}
+	private CellResponse(){
 
-	public CellResponse(int row, int col, Integer value) {
-		this.row = row;
-		this.col = col;
-		this.hidden = false;
-		this.value = value;
 	}
 
 	public int getRow() {
@@ -32,15 +21,48 @@ public class CellResponse {
 		return col;
 	}
 
-	public boolean isHidden() {
-		return hidden;
-	}
-
 	public Integer getValue() {
 		return value;
 	}
 
 	public CellContent getContent() {
 		return content;
+	}
+
+	public static class Builder {
+
+		int row;
+		int col;
+		private Integer value;
+		private CellContent content;
+
+		public Builder withRow(int row){
+			this.row = row;
+			return this;
+		}
+
+		public Builder withCol(int col){
+			this.col = col;
+			return this;
+		}
+
+		public Builder withValue(Integer value){
+			this.value = value;
+			return this;
+		}
+
+		public Builder withCellContent(CellContent content){
+			this.content = content;
+			return this;
+		}
+
+		public CellResponse build() {
+			CellResponse cellResponse = new CellResponse();
+			cellResponse.row = this.row;
+			cellResponse.col = this.col;
+			cellResponse.value = this.value;
+			cellResponse.content = this.content;
+			return cellResponse;
+		}
 	}
 }
--- src/main/java/com/deviget/minesweeper/domain/domain2api/CellsResponseTransformer.java
+++ src/main/java/com/deviget/minesweeper/domain/domain2api/CellsResponseTransformer.java
@@ -4,30 +4,25 @@ import com.deviget.minesweeper.api.response.CellResponse;
 import com.deviget.minesweeper.model.Cell;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class CellsResponseTransformer {
 
-	public List<CellResponse> transform(Cell[][] board) {
-		int rows = board.length;
-		int cols = board[0].length;
-
-		List<CellResponse> response = new ArrayList<>();
-		for(int row = 0; row< rows; row++){
-			for(int col = 0; col< cols; col++){
-				Cell cell = board[row][col];
-				response.add(this.transform(cell));
-			}
-		}
-		return response;
+	public List<CellResponse> transform(List<Cell> affectedCells) {
+		return affectedCells.stream()
+				.map(this::transform)
+				.collect(Collectors.toList());
 	}
 
 	public CellResponse transform(Cell cell) {
-		if(cell.isHidden()){
-			return new CellResponse(cell.getRow(),cell.getCol(),cell.getContent());
-		}
-		return new CellResponse(cell.getRow(),cell.getCol(),cell.getValue());
+		Integer value = cell.isHidden()? -1 : cell.getValue();
+		return new CellResponse.Builder()
+				.withRow(cell.getRow())
+				.withCol(cell.getCol())
+				.withCellContent(cell.getContent())
+				.withValue(value)
+				.build();
 	}
 }
