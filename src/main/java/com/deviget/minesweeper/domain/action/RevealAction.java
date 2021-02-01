0066c2ee7f1c85f6c32bc6e7febc3fde4df1b282
QuiqueScampini <enrique.scampini@despegar.com>
2021-01-31 21:47:39 -0300

    RevealAction first approach

--- src/main/java/com/deviget/minesweeper/domain/action/RevealAction.java
+++ src/main/java/com/deviget/minesweeper/domain/action/RevealAction.java
@@ -6,11 +6,14 @@ import com.deviget.minesweeper.model.CellContent;
 import com.deviget.minesweeper.model.Game;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
-public class RevealAction implements GameAction {
+public class RevealAction extends GameAction {
 
 	@Override
 	public List<Cell> execute(ActionRequest actionRequest, Game game) {
@@ -20,17 +23,45 @@ public class RevealAction implements GameAction {
 		if(!cell.isHidden() || cell.getContent().equals(CellContent.FLAG))
 			return Collections.emptyList();
 
+		Integer value = cell.getValue();
+		if(value < 0 ){
+			throw new RuntimeException("Game Over");
+		}
+
 		return this.revealCell(cell,game);
 	}
 
 	private List<Cell> revealCell(Cell cell, Game game) {
-		return Collections.emptyList();
+		List<Cell> result = new ArrayList<>();
+		cell.setHidden(false);
+		result.add(cell);
+		if(cell.getValue().equals(0)){
+			result.addAll(this.revealAdjacent(cell,game));
+		}
+		return result;
 	}
 
-	private Cell retrieveCell(Game game, ActionRequest actionRequest) {
-		int row = actionRequest.getRow();
-		int col = actionRequest.getCol();
-		//TODO validate it exists
-		return game.getBoard()[row][col];
+	private List<Cell> revealAdjacent(Cell cell, Game game) {
+		return this.retrieveAdjacent(cell,game).stream()
+				.map( adjacentCell -> this.revealCell(adjacentCell,game))
+				.flatMap(Collection::stream)
+				.collect(Collectors.toList());
+	}
+
+	private List<Cell> retrieveAdjacent(Cell cell, Game game) {
+		List<Cell> adjacent = new ArrayList<>();
+
+		Cell[][] board = game.getBoard();
+
+		int cellRow = cell.getRow();
+		int celCol = cell.getCol();
+		for(int row = cellRow -1; row < cellRow + 1; row ++){
+			for(int col=celCol -1; col <= celCol +1; col++){
+				if(row > 0 && col > 0) {
+					adjacent.add(board[row][col]);
+				}
+			}
+		}
+		return adjacent;
 	}
 }
