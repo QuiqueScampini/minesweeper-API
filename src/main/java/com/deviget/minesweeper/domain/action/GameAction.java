bdc15d32013913851c363949a5c07e64c216e439
QuiqueScampini <enrique.scampini@despegar.com>
2021-01-31 21:55:34 -0300

    GameAction interface again

--- src/main/java/com/deviget/minesweeper/domain/action/GameAction.java
+++ src/main/java/com/deviget/minesweeper/domain/action/GameAction.java
@@ -6,14 +6,8 @@ import com.deviget.minesweeper.model.Game;
 
 import java.util.List;
 
-public abstract class GameAction {
+public interface GameAction {
 
-	public abstract List<Cell> execute(ActionRequest request, Game game);
+	List<Cell> execute(ActionRequest request, Game game);
 
-	protected Cell retrieveCell(Game game, ActionRequest actionRequest) {
-		int row = actionRequest.getRow();
-		int col = actionRequest.getCol();
-		//TODO validate it exists
-		return game.getBoard()[row][col];
-	}
 }
