8e0cb986ebf299c09b18e52bcaaa6a61dc968072
QuiqueScampini <enrique.scampini@despegar.com>
2021-02-01 21:46:22 -0300

    InternalException and Handler

--- src/main/java/com/deviget/minesweeper/controller/handler/MinesWeeperExceptionHandler.java
+++ src/main/java/com/deviget/minesweeper/controller/handler/MinesWeeperExceptionHandler.java
@@ -2,6 +2,7 @@ package com.deviget.minesweeper.controller.handler;
 
 import com.deviget.minesweeper.api.response.ErrorResponse;
 import com.deviget.minesweeper.controller.exception.RequestValidationException;
+import com.deviget.minesweeper.domain.exception.InternalException;
 import com.deviget.minesweeper.domain.exception.NotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -28,7 +29,13 @@ public class MinesWeeperExceptionHandler extends ResponseEntityExceptionHandler 
 		return handleExceptionInternal(ex, errorResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 
-	@ExceptionHandler(value={RuntimeException.class })
+	@ExceptionHandler(value={InternalException.class})
+	protected ResponseEntity<Object> handleRuntimeException(InternalException ex, WebRequest request) {
+		ErrorResponse errorResponse = new ErrorResponse(INTERNAL_SERVER_ERROR, ex);
+		return handleExceptionInternal(ex, errorResponse, new HttpHeaders(), INTERNAL_SERVER_ERROR, request);
+	}
+
+	@ExceptionHandler(value={RuntimeException.class})
 	protected ResponseEntity<Object> handleRuntimeException(RuntimeException ex, WebRequest request) {
 		ErrorResponse errorResponse = new ErrorResponse(INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR.getReasonPhrase());
 		return handleExceptionInternal(ex, errorResponse, new HttpHeaders(), INTERNAL_SERVER_ERROR, request);
---
+++ src/main/java/com/deviget/minesweeper/domain/exception/InternalException.java
@@ -0,0 +1,8 @@
+package com.deviget.minesweeper.domain.exception;
+
+public class InternalException extends RuntimeException {
+
+	public InternalException(String message) {
+		super(message);
+	}
+}
