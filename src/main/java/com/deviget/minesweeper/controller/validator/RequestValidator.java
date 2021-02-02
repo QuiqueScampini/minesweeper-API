3a4db2327b484e6a1ce49a04c56dd541c40a398a
QuiqueScampini <enrique.scampini@despegar.com>
2021-02-02 19:50:31 -0300

    Min and Max cols, rows and Mines definition

--- src/main/java/com/deviget/minesweeper/controller/validator/RequestValidator.java
+++ src/main/java/com/deviget/minesweeper/controller/validator/RequestValidator.java
@@ -10,15 +10,25 @@ import org.springframework.stereotype.Component;
 public class RequestValidator {
 
 	private final int minCols;
+	private final int maxRows;
 	private final int minRows;
+	private final int maxCols;
 	private final int minMines;
+	private final int maxMines;
 
 	public RequestValidator( @Value("${game.min.rows}") int minRows,
+							 @Value("${game.max.rows}") int maxRows,
 							 @Value("${game.min.cols}") int minCols,
-							 @Value("${game.min.mines}") int minMines){
-		this.minCols = minCols;
+							 @Value("${game.max.cols}") int maxCols,
+							 @Value("${game.min.mines}") int minMines,
+							 @Value("${game.max.mines}") int maxMines){
 		this.minRows = minRows;
+		this.maxRows = maxRows;
+		this.minCols = minCols;
+		this.maxCols = maxCols;
 		this.minMines = minMines;
+		this.maxMines = maxMines;
+
 	}
 
 	public void validateRequest(GameRequest gameRequest) {
@@ -26,14 +36,14 @@ public class RequestValidator {
 		int rows = gameRequest.getRows();
 		int mines = gameRequest.getMines();
 
-		if(rows < this.minRows)
-			throw new RequestValidationException("Can't create game: Minimum rows size is " + this.minRows);
+		if(rows < this.minRows || rows > maxRows)
+			throw new RequestValidationException("Can't create game: Rows must be between " + this.minRows + " and " + this.maxRows);
 
-		if(cols < this.minCols)
-			throw new RequestValidationException("Can't create game: Minimum cols size is " + this.minCols);
+		if(cols < this.minCols || cols > maxCols)
+			throw new RequestValidationException("Can't create game: Cols must be between " + this.minCols + " and " + this.maxCols);
 
-		if(mines < this.minMines)
-			throw new RequestValidationException("Can't create game: Minimum mines size is " + this.minMines);
+		if(mines < this.minMines || mines > maxMines)
+			throw new RequestValidationException("Can't create game: Mines must be between " + this.minMines + " and " + this.maxMines);
 
 		if(mines >= cols * rows)
 			throw new RequestValidationException("Can't create game: There are more mines than cells");
