17aba071b536eaebf08c958545191c0682aef8d9
QuiqueScampini <enrique.scampini@despegar.com>
2021-02-01 21:13:42 -0300

    To be implemented GameTimeResolver

---
+++ src/main/java/com/deviget/minesweeper/domain/domain2api/GameTimeResolver.java
@@ -0,0 +1,13 @@
+package com.deviget.minesweeper.domain.domain2api;
+
+import com.deviget.minesweeper.model.Game;
+import org.springframework.stereotype.Component;
+
+@Component
+class GameTimeResolver {
+
+	public int resolve(Game game) {
+		//TODO Implement
+		return 0;
+	}
+}
