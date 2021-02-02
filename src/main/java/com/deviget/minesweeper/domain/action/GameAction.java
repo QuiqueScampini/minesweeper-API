3a36c194b98c99d1532c12659384e298d6289421
QuiqueScampini <enrique.scampini@despegar.com>
2021-02-01 22:40:26 -0300

    GameAction Refactor

--- src/main/java/com/deviget/minesweeper/domain/action/GameAction.java
+++ src/main/java/com/deviget/minesweeper/domain/action/GameAction.java
@@ -1,13 +1,10 @@
 package com.deviget.minesweeper.domain.action;
 
 import com.deviget.minesweeper.api.request.ActionRequest;
-import com.deviget.minesweeper.model.Cell;
 import com.deviget.minesweeper.model.Game;
-
-import java.util.List;
 
 public interface GameAction {
 
-	List<Cell> execute(ActionRequest request, Game game);
+	void execute(ActionRequest request, Game game);
 
 }
