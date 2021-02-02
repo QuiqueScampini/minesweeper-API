6260fa126323aa0848a19d304378e9cf9a574631
QuiqueScampini <enrique.scampini@despegar.com>
2021-02-02 19:40:33 -0300

    Find by User

--- src/main/java/com/deviget/minesweeper/repository/GameRepository.java
+++ src/main/java/com/deviget/minesweeper/repository/GameRepository.java
@@ -3,5 +3,13 @@ package com.deviget.minesweeper.repository;
 import com.deviget.minesweeper.model.Game;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface GameRepository extends CrudRepository<Game,Integer> {
+
+	@Override
+	List<Game> findAll();
+
+	List<Game> findAllByUser(String user);
+
 }
